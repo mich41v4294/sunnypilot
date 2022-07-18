c04d86a7f43c74d6c17c5ec06a91d59bf897e6a0
Michal Varga <40143965+mich41v4294@users.noreply.github.com>
2022-07-18 21:14:35 +0200

    disable gps error

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -296,9 +296,9 @@ class Controls:
       self.events.add(EventName.vehicleModelInvalid)
     if not self.sm['lateralPlan'].mpcSolutionValid:
       self.events.add(EventName.plannerError)
-    if not self.sm['liveLocationKalman'].sensorsOK and not NOSENSOR:
-      if self.sm.frame > 5 / DT_CTRL:  # Give locationd some time to receive all the inputs
-        self.events.add(EventName.sensorDataInvalid)
+    #if not self.sm['liveLocationKalman'].sensorsOK and not NOSENSOR:
+    #  if self.sm.frame > 5 / DT_CTRL:  # Give locationd some time to receive all the inputs
+    #    self.events.add(EventName.sensorDataInvalid)
     if not self.sm['liveLocationKalman'].posenetOK:
       self.events.add(EventName.posenetInvalid)
     if not self.sm['liveLocationKalman'].deviceStable:
